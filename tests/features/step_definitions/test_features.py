596681cbcf5a10175fdc34e087c53b20bb18a657
Anakin Childerhose <anakin@childerhose.ca>
2026-07-03 21:13:31 -0400

    chore: mv test_features.py out of step_definitions folder

--- tests/features/step_definitions/test_features.py
+++
@@ -1,6 +0,0 @@
-from pytest_bdd import scenarios
-
-scenarios('../hover/standard-properties.feature')
-scenarios('../hover/standard-nodes.feature')
-scenarios('../hover/interrupt-properties.feature')
-scenarios('../hover/nexus-properties.feature')
---
+++ tests/features/test_features.py
@@ -0,0 +1,6 @@
+from pytest_bdd import scenarios
+
+scenarios('hover/standard-properties.feature')
+scenarios('hover/standard-nodes.feature')
+scenarios('hover/interrupt-properties.feature')
+scenarios('hover/nexus-properties.feature')
