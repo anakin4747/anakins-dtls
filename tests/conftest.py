924ef0634331f826592d61354499e332c1a3ba27
Anakin Childerhose <anakin@childerhose.ca>
2026-07-12 11:27:52 -0400

    fix: remove pytest hook for printing test line number

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,3 @@ _nodeid_lines: dict[str, int] = {}
 def pytest_collection_modifyitems(config, items):
     for item in items:
         _nodeid_lines[item.nodeid] = item.location[1] + 1
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_logreport(report):
-    line = _nodeid_lines.get(report.nodeid)
-    if line is not None:
-        path, _, name = report.nodeid.partition("::")
-        report.nodeid = f"{path}:{line}: {name}"
-    yield
