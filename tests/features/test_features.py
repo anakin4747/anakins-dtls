23bbbf742ceebd9ca16987b31c25fb150a44497d
Anakin Childerhose <anakin@childerhose.ca>
2026-07-12 23:18:56 -0400

    test(bdd): gather feature scenarios by globbing feature files

--- tests/features/test_features.py
+++ tests/features/test_features.py
@@ -1,11 +1,3 @@
 from pytest_bdd import scenarios
 
-scenarios('hover/standard-properties.feature')
-scenarios('hover/standard-nodes.feature')
-scenarios('hover/chapter4-bindings.feature')
-scenarios('hover/dts-source-language.feature')
-scenarios('hover/interrupt-properties.feature')
-scenarios('hover/nexus-properties.feature')
-scenarios('hover/kernel-binding.feature')
-scenarios('kernel-source/resolution.feature')
-scenarios('kernel-source/config-file.feature')
+scenarios('.')
