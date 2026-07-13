13a3345eb1fb5861a6d3b685a97983333ab42e1d
Anakin Childerhose <anakin@childerhose.ca>
2026-07-12 23:23:21 -0400

    chore: remove dead code

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-_nodeid_lines: dict[str, int] = {}
-
-
-@pytest.hookimpl(trylast=True)
-def pytest_collection_modifyitems(config, items):
-    for item in items:
-        _nodeid_lines[item.nodeid] = item.location[1] + 1
