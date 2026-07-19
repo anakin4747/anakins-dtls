39a6b583dbc53304ccfa0677178ec2ff26bfa89b
Anakin Childerhose <anakin@childerhose.ca>
2026-07-19 09:09:19 -0400

    refactor: use flake-utils for eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -1,73 +1,54 @@
 {
   description = "Hello world Lua CLI packaged with Nix";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        with pkgs; {
-          default = stdenvNoCC.mkDerivation {
-            pname = "lua-hello-world";
-            version = "0.1.0";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs; {
+        packages.default = stdenvNoCC.mkDerivation {
+          pname = "lua-hello-world";
+          version = "0.1.0";
 
-            src = ./.;
-            dontBuild = true;
+          src = ./.;
+          dontBuild = true;
 
-            nativeBuildInputs = [ lua ];
+          nativeBuildInputs = [ lua ];
 
-            installPhase = ''
-              runHook preInstall
-              patchShebangs hello.lua
-              install -Dm755 hello.lua $out/bin/lua-hello-world
-              runHook postInstall
-            '';
-          };
-        });
-
-      checks = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        with pkgs; {
-          default = runCommand "lua-hello-world-check" {
-            nativeBuildInputs = [ gnumake lua luaPackages.busted cocogitto ];
-            src = ./.;
-            IN_NIX_SHELL = 1;
-          } ''
-            cd "$src"
-            make test
-            touch "$out"
+          installPhase = ''
+            runHook preInstall
+            patchShebangs hello.lua
+            install -Dm755 hello.lua $out/bin/lua-hello-world
+            runHook postInstall
           '';
-        });
+        };
 
-      apps = forAllSystems (system: {
-        default = {
+        checks.default = runCommand "lua-hello-world-check" {
+          nativeBuildInputs = [ gnumake lua luaPackages.busted cocogitto ];
+          src = ./.;
+          IN_NIX_SHELL = 1;
+        } ''
+          cd "$src"
+          make test
+          touch "$out"
+        '';
+
+        apps.default = {
           type = "app";
           program = "${self.packages.${system}.default}/bin/lua-hello-world";
         };
-      });
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in with pkgs; {
-          default = mkShell {
-            packages = [
-              lua
-              luaPackages.busted
-              cocogitto
-              gnumake
-            ];
-          };
-        }
-      );
-    };
+        devShells.default = mkShell {
+          packages = [
+            lua
+            luaPackages.busted
+            cocogitto
+            gnumake
+          ];
+        };
+      });
 }
