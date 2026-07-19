7310eff2f46152579a793293568cad5a50e64539
Anakin Childerhose <anakin@childerhose.ca>
2026-07-19 15:35:06 -0400

    refactor: add a syntax sugar function to support gf on test fixtures

--- tests/anakins-dtls_tests.lua
+++ tests/anakins-dtls_tests.lua
@@ -6,6 +6,11 @@ local it = busted.it
 package.path = "./lua/?.lua;" .. package.path
 local dtls = require("anakins-dtls")
 
+local function row_col(filename_linenumber)
+    local row, col = filename_linenumber:match(":(%d+):(%d+)")
+    return tonumber(row), tonumber(col)
+end
+
 local dts_locations = {
     { name = "in-tree", path = "arch/arm64/boot/dts/freescale/custom.dts" },
     { name = "out-of-tree", path = "custom.dts" },
@@ -20,67 +25,58 @@ for _, location in ipairs(dts_locations) do
     describe(location.name, function()
         describe("/", function()
             it("indicates if in a root node", function()
-                ctx.row = 16
-                ctx.col = 2
+                ctx.row, ctx.col = row_col("tests/custom.dts:16:2")
                 assert(dtls.in_a_root_node(ctx))
             end)
 
             it("does not mistake outside of a root node for inside a root node", function()
-                ctx.row = 0
-                ctx.col = 0
+                ctx.row, ctx.col = row_col("tests/custom.dts:1:1")
                 assert(not dtls.in_a_root_node(ctx))
 
-                ctx.row = 114
+                ctx.row, ctx.col = row_col("tests/custom.dts:114:1")
                 assert(not dtls.in_a_root_node(ctx))
             end)
 
             it("indicates if not on a root node", function()
-                ctx.row = 0
-                ctx.col = 0
+                ctx.row, ctx.col = row_col("tests/custom.dts:1:1")
                 assert(not dtls.on_a_root_node(ctx))
             end)
 
             it("indicates if on a root node's opening bracket", function()
-                ctx.row = 20
-                ctx.col = 10
+                ctx.row, ctx.col = row_col("tests/custom.dts:20:10")
                 assert(dtls.on_a_root_node(ctx))
 
-                ctx.col = 9 -- The space before should also count
+                ctx.row, ctx.col = row_col("tests/custom.dts:20:9") -- The space before should also count
                 assert(dtls.on_a_root_node(ctx))
             end)
 
             it("indicates if on a root node's closing bracket", function()
-                ctx.row = 45
-                ctx.col = 2
+                ctx.row, ctx.col = row_col("tests/custom.dts:45:2")
                 assert(dtls.on_a_root_node(ctx))
 
-                ctx.col = 3 -- The semicolon after should also count
+                ctx.row, ctx.col = row_col("tests/custom.dts:45:3") -- The semicolon after should also count
                 assert(dtls.on_a_root_node(ctx))
             end)
         end)
 
         describe("/aliases", function()
             it("identifies if in an /aliases node", function()
-                ctx.row = 21
-                ctx.col = 3
+                ctx.row, ctx.col = row_col("tests/custom.dts:21:3")
                 assert(dtls.in_an_aliases_node(ctx))
             end)
 
             it("indicates if in a root node even in an /aliases node", function()
-                ctx.row = 21
-                ctx.col = 3
+                ctx.row, ctx.col = row_col("tests/custom.dts:21:3")
                 assert(dtls.in_a_root_node(ctx))
             end)
 
             it("doesn't mistake inside the root node for inside an /aliases node", function()
-                ctx.row = 17
-                ctx.col = 2
+                ctx.row, ctx.col = row_col("tests/custom.dts:17:2")
                 assert(not dtls.in_an_aliases_node(ctx))
             end)
 
             it("doesn't mistake being inside a /child/aliases node for being inside an /aliases node", function()
-                ctx.row = 110
-                ctx.col = 13
+                ctx.row, ctx.col = row_col("tests/custom.dts:110:13")
                 assert(not dtls.in_an_aliases_node(ctx))
             end)
         end)
